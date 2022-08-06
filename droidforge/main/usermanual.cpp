22c6331c663d09b681338e05f9cc077dd29e5546
Mathias Kettner <mk@mathias-kettner.de>
2022-08-06 23:10:46 +0200

    TODO

--- droidforge/main/usermanual.cpp
+++ droidforge/main/usermanual.cpp
@@ -71,6 +71,8 @@ void UserManual::openExternally()
 }
 void UserManual::showCircuit(const QString &circuit)
 {
+    shout << "Man zu circuit" << circuit;
+    shout << "Das sollte sein auf Seite" << the_firmware->circuitManualPage(circuit);
     show();
     jumpToPage(the_firmware->circuitManualPage(circuit));
 }
