694ccb9d0522f448df28a3d4782c700105ef3abd
Mathias Kettner <mk@mathias-kettner.de>
2022-10-15 10:21:44 +0200

    Bumped version to 1.0

--- droidforge/main/tuning.h
+++ droidforge/main/tuning.h
@@ -7,7 +7,7 @@
 #ifdef QT_DEBUG
 #define APPLICATION_VERSION                  ("SNAPSHOT " __DATE__)
 #else
-#define APPLICATION_VERSION                  "0.9"
+#define APPLICATION_VERSION                  "1.0"
 #endif
 #define LICENSE_URL                          "https://www.gnu.org/licenses/gpl.txt"
 #define DISCORD_URL                          "https://discord.com/invite/9TUcRmH"
