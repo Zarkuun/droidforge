eb1ce76b028b1c80c6cb2f27d1d352de0db185b5
Mathias Kettner <mk@mathias-kettner.de>
2023-02-01 18:52:40 +0100

    PDF-Viewer geht wieder

--- forgecmake/keycapturelineedit.cpp
+++ forgecmake/keycapturelineedit.cpp
@@ -12,15 +12,15 @@ void KeyCaptureLineEdit::keyPressEvent(QKeyEvent *event)
 {
     int key = event->key();
     if (key == Qt::Key_Up
-        || key == Qt::Key_Down
-        || key == Qt::Key_Left
-        || key == Qt::Key_Right
-        || key == Qt::Key_Home
-        || key == Qt::Key_End
-        || key == Qt::Key_Space)
+            || key == Qt::Key_Down
+            || key == Qt::Key_Left
+            || key == Qt::Key_Right
+            || key == Qt::Key_Home
+            || key == Qt::Key_End
+            || key == Qt::Key_Space)
     {
-       emit keyPressed(event->key());
+        emit keyPressed(event->key());
     }
     else
-       QLineEdit::keyPressEvent(event);
+        QLineEdit::keyPressEvent(event);
 }
