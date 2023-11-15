4dbc4a8e8dba5561d2345c6a8e09fa8ffbe60992
Mathias Kettner <mk@mathias-kettner.de>
2023-11-15 17:47:19 +0100

    Minor code cleanup

--- droidforge/main/main.cpp
+++ droidforge/main/main.cpp
@@ -18,7 +18,7 @@
 
 // http://www.mardy.it/blog/2019/10/implementing-open-with-on-macos-with-qt.html
 
-MainWindow *hirn_mainwindow = 0;
+MainWindow *the_mainwindow = 0;
 
 class MyApplication : public QApplication
 {
@@ -30,12 +30,8 @@ public:
     {
         if (event->type() == QEvent::FileOpen) {
             QFileOpenEvent *openEvent = static_cast<QFileOpenEvent *>(event);
-            if (hirn_mainwindow)
-                hirn_mainwindow->theOperator()->openFileFromExternal(openEvent->file());
-            // QMessageBox::about( 0,
-            //             tr("Man will eine Datei Laden"),
-            //             openEvent->file());
-            // qDebug() << "Open file" << openEvent->file();
+            if (the_mainwindow)
+                the_mainwindow->theOperator()->openFileFromExternal(openEvent->file());
         }
 
         return QApplication::event(event);
@@ -80,7 +76,7 @@ int main(int argc, char *argv[])
     else if (settings.contains("lastfile"))
         initialFilename = settings.value("lastfile").toString();
     MainWindow *mainWindow = new MainWindow(initialFilename);
-    hirn_mainwindow = mainWindow;
+    the_mainwindow = mainWindow;
     mainWindow->show();
 
 #ifdef QT_DEBUG
