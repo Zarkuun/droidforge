c41557a77838b462e35e4bb18de4817384fa737c
Mathias Kettner <mk@mathias-kettner.de>
2023-12-22 10:49:38 +0100

    Switch to firmware magenta-25

--- droidforge/main/tuning.h
+++ droidforge/main/tuning.h
@@ -7,7 +7,7 @@
 #ifdef QT_DEBUG
 #define APPLICATION_VERSION                  ("SNAPSHOT " __DATE__)
 #else
-#define APPLICATION_VERSION                  "1.2.2-beta1"
+#define APPLICATION_VERSION                  "1.2.3-beta1"
 #endif
 #define LICENSE_URL                          "https://www.gnu.org/licenses/gpl.txt"
 #define DISCORD_URL                          "https://discord.com/invite/9TUcRmH"
