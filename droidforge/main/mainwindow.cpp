12d2a0b02e89e6bf31cd333d86127c747f739a8b
Mathias Kettner <mk@mathias-kettner.de>
2025-10-25 16:29:44 +0200

    Show Qt version in About

--- droidforge/main/mainwindow.cpp
+++ droidforge/main/mainwindow.cpp
@@ -791,6 +791,8 @@ void MainWindow::about()
 
                            "Copyright Mathias Kettner 2025.\n\n"
 
+                           "Built with Qt version %3.\n\n"
+
                            "This program is free software: you can redistribute it and/or modify it under "
                            "the terms of the GNU General Public License as published by the Free Software "
                            "Foundation, either version 3 of the License, or (at your option) any later version."
@@ -800,7 +802,7 @@ void MainWindow::about()
 
                            "You should have received a copy of the GNU General Public License along with this program. "
                            "If not, see <https://www.gnu.org/licenses/>. ")
-                        .arg(APPLICATION_VERSION, firmware_version));
+                        .arg(APPLICATION_VERSION, firmware_version, QT_VERSION_STR));
 }
 void MainWindow::editPreferences()
 {
