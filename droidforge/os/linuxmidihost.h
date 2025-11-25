55ba69a90a165039fb57a476438a4adabf2979ad
vladimir <vladimir@pantelic.net>
2025-11-25 22:15:46 +0100

    implement X7 detection and Sysex sending for Linux
    Signed-off-by: vladimir <vladimir@pantelic.net>

--- droidforge/os/linuxmidihost.h
+++ droidforge/os/linuxmidihost.h
@@ -6,6 +6,8 @@
 #include <stdint.h>
 #include <QString>
 
+struct alsaSeq;
+
 class LinuxMIDIHost : public MIDIHost
 {
 public:
@@ -14,7 +16,12 @@ public:
     QString sendPatch(const Patch *patch) override;
 
 private:
-    int findX7Device() const;
+    int findX7(int *) const;
+    bool connect(int, int);
+    void disconnect(int, int);
+    void send(uint8_t*, unsigned);
+
+    alsaSeq *alsa;
 };
 
 #endif // LINUXMIDIHOST_H
